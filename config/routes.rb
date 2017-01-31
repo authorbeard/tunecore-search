ea10ea36f7acb0453ab07eb8ecd13de2a078755f
authorbeard <msnolan@gmail.com>
2017-01-31 11:23:47 -0500

    test passing

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'search#home'
 
   get 'home', to: "search#home", as: "home"
-  # post '/search', to: "search#search", as: 'search'
+  post '/search', to: "search#search"#, as: 'search'
 end
