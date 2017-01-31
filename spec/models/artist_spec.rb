b857e970ec0d8a754ac1afacd3725f8842113b6c
authorbeard <msnolan@gmail.com>
2017-01-31 14:07:51 -0500

    added artist creation to before loop. Began testing has_many relationship for songs

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,25 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Artist do
 
+  before :each do
+    @artist = Artist.create(name: Faker::Name.name)
+  end
+
   it 'Each artist has a name' do
-    artist = Artist.create(name: "David Bowie")
-    expect(artist.name).to eq("David Bowie")
+    expect(@artist.name).to_not eq(nil)
   end
 
   it 'An artist can have an album' do
-    artist = Artist.create(name: "David Bowie")
-    artist.albums << Album.new(name: "Black Star")
-    expect(artist.albums.count).to eq(1)
+    # artist = Artist.create(name: "David Bowie")
+    @artist.albums << Album.new(name: "Black Star")
+    expect(@artist.albums.count).to eq(1)
   end
 
   it 'An artist can have many albums' do
-    artist=Artist.create(name: Faker::Name.name)
+    # artist=Artist.create(name: Faker::Name.name)
     10.times{ Album.create(name: Faker::Name.name)}
     Album.all.each{|a| 
-      artist.albums << a
-      artist.save
+      @artist.albums << a
+      @artist.save
       }
-    expect(artist.albums.count).to eq(10)
+    expect(@artist.albums.count).to eq(10)
+  end
+
+  it 'An artist can have many songs' do
+    assert false
   end
 
 
