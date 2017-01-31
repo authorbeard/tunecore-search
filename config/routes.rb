a52c8e622ebf918d09b71c741d7f8bb42b43cc10
authorbeard <msnolan@gmail.com>
2017-01-31 11:09:01 -0500

    changed rspec config to use 'doc' formatter for verbose output

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  
-  # root 'search#home'
+
+  root 'search#home'
+
+  get 'home', to: "search#home", as: "home"
   # post '/search', to: "search#search", as: 'search'
 end
