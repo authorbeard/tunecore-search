a9d2d803b1b1858ea6302ca3e29348effffd184e
authorbeard <msnolan@gmail.com>
2017-01-31 10:37:05 -0500

    added dataase cleaner gem, initial model migration setup, changed rspec config to use database cleaner

---
+++ spec/routing/routing_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+Rspec.describe 'routing' do 
+
+  it 'should have a home action' do
+
+  end
+
+  it 'root should point to search#home' do
+
+  end
+
+  it 'should only accept GET requests for home' do
+
+  end
+
+  it 'should route queries to search#search' do
+
+  end
+
+  it 'The search action should only accept post requests' do
+
+  end
+
+  it 'The search action should respond to json by default' do
+
+  end
+
+
+
+
+end
