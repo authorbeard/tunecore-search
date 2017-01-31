7ac549c68c4512d90c64ce3b73504436964c78bf
authorbeard <msnolan@gmail.com>
2017-01-31 11:16:44 -0500

    tested routability, home path, root path. All passing

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -2,30 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'routing' do 
 
-  it 'should have a home action' do
+  it 'routes to "/home"' do
+    expect(:get => '/home').to be_routable
+  end
+
+  it 'should have a home path' do
     expect(:get => home_path).to route_to(:controller => "search", :action => "home")
-
   end
 
-  it 'root should point to search#home' do
-
+  it 'root should route to home path' do
+  byebug
+    expect(:get => '/').to route_to(:controller => "search", :action => "home")
   end
 
-  it 'should only accept GET requests for home' do
+  # it 'should only accept GET requests for home' do
 
-  end
+  # end
 
-  it 'should route queries to search#search' do
+  # it 'should route queries to search#search' do
 
-  end
+  # end
 
-  it 'The search action should only accept post requests' do
+  # it 'The search action should only accept post requests' do
 
-  end
+  # end
 
-  it 'The search action should respond to json by default' do
+  # it 'The search action should respond to json by default' do
 
-  end
+  # end
 
 
 
