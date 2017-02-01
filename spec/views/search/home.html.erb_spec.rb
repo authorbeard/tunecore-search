24645fd2726bfebb0cd0ccce28d037ffd7c460dd
authorbeard <msnolan@gmail.com>
2017-02-01 11:57:32 -0500

    first search box test passing

--- spec/views/search/home.html.erb_spec.rb
+++ spec/views/search/home.html.erb_spec.rb
@@ -24,31 +24,49 @@ RSpec.describe "search/home.html.erb" do
       expect(page).to have_css('div.container form#search-form')
     end
 
-    it "Has a form group for the search box & label" do
-      search = page.find('.container form#search-form')
-      expect(search).to have_css('div.form-group')
+    context "Query Input field & label" do
+      it "Has a form group for the search box & label" do
+        search = page.find('.container form#search-form')
+        expect(search).to have_css('div.form-group')
+      end
+
+      it "Places the form group in a row" do
+        search = page.find('div.container form#search-form')
+        expect(search).to have_css('.form-group.row')
+      end
+
+      context "Label" do
+        it "Has a label element for the keyword input field" do
+          search = page.find('div#search-input-group')
+          expect(search).to have_css('label', :count => 1)
+        end
+
+        it "The label has responsive classes for column, field reference" do
+          search = page.find('div#search-input-group')
+          expect(search).to have_css('label.col-xs-2.col-form-label')
+          expect(search).to have_css('label[for=query-terms]', :count=>1)
+        end
+
+        it "The label refers to the keyword input field box" do
+          label = page.find('label[for=query-terms]')
+          expect(label).to have_content("Search terms:")
+        end
+      end
+        
+      context "Search Box" do
+        it "Places the search input field in its own responsive column" do
+          search = page.find('div#search-input-group')
+          expect(search).to have_css('div.col-xs-10#search-box')
+        end
+
+        it "Renders the search box as an input element" do
+          expect
+        end
+      end
+
     end
 
-    it "Places the form group in a row" do
-      search = page.find('div.container form#search-form')
-      expect(search).to have_css('.form-group.row')
-    end
 
-    it "Has a label element for the keyword input field" do
-      search = page.find('div#search-box')
-      expect(search).to have_css('label', :count => 1)
-    end
-
-    it "The label has responsive classes for column, field reference" do
-      search = page.find('div#search-box')
-      expect(search).to have_css('label.col-xs-2.col-form-label')
-      expect(search).to have_css('label[for=query-terms]', :count=>1)
-    end
-
-    it "The label refers to the keyword input field box" do
-      label = page.find('label#search-box-label')
-      expect(label).to have_content("Search terms:")
-    end
 
   end
 
