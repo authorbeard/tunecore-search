e27940c25f08b3af378a29ec9a664ea20187df7e
authorbeard <msnolan@gmail.com>
2017-02-01 10:20:43 -0500

    passing test for existence of search form

--- spec/views/search/home.html.erb_spec.rb
+++ spec/views/search/home.html.erb_spec.rb
@@ -3,34 +3,25 @@ require 'capybara/rails'
 
 RSpec.describe "search/home.html.erb" do
 
-  it "Welcomes the User" do
+  before :each do
     visit home_path
+  end
+
+  it "Welcomes the User" do
     expect(page).to have_content("Welcome to Tunecore Search") 
   end
 
-  it "Contains basic instructions" do
-    visit home_path
-    expect(page).to have_css('div.instructions')
-  end
-
-  it "Explains the search options" do
-    visit home_path
-    instructions = page.find('div.instructions')
-  byebug
-  end
-
-
-  context "Before a user has searched for anything" do
-    it "Has a responsive grid" do
-      assert false
+  context "It has a responsive grid layout" do
+    it "renders page contents inside a container" do
+      expect(page).to have_css('div.container')
     end
 
-
-
-
-
-
+    it "renders the search form inside the container" do
+      expect(page).to have_css('.container form.search-form')
+    
+    end
   end
+
 
 
   
