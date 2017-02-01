1646b812debb5ee1a9fee749e92ad7a23e1adaca
authorbeard <msnolan@gmail.com>
2017-02-01 09:32:08 -0500

    first view path test written/failing

--- spec/views/search/home.html.erb_spec.rb
+++ spec/views/search/home.html.erb_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
+require 'capybara/rails'
 
-RSpec.describe "search/search.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "search/home.html.erb" do
+
+  it "Welcomes the User" do
+    visit home_path
+    expect(page).to have_content("Welcome to Tunecore Search") 
+  end
+
+  context "Before a user has searched for anything" do
+    it "Has a responsive grid" do
+      assert false
+    end
+
+
+
+
+
+
+  end
+
+
+  
+
+
+
+
 end
