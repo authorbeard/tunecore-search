4f0ea0efa9d437688439e5d5b96c384e8b562fbc
authorbeard <msnolan@gmail.com>
2017-02-06 11:24:24 -0500

    fixed final typos in label test

--- spec/views/search/home.html.erb_spec.rb
+++ spec/views/search/home.html.erb_spec.rb
@@ -134,8 +134,8 @@ RSpec.describe "search/home.html.erb" do
       it "Labels should list all available search fields" do
         labels = page.all('div#inclusive-search label')   
         expect(labels.first.text).to eq("Artist Name")
-        expect(labels[1].text).to eq("Artist Name")
-        expect(labels[2].text).to eq("Artist Name")
+        expect(labels[1].text).to eq("Album Title")
+        expect(labels[2].text).to eq("Song Title")
         
       end
 
