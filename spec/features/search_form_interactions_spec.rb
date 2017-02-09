8f992d5413eeb20dcdbffce2ded93fc798f93f7b
authorbeard <msnolan@gmail.com>
2017-02-09 11:20:20 -0500

    refactored feature specs before hook to select Custom radio button before each test.

--- spec/features/search_form_interactions_spec.rb
+++ spec/features/search_form_interactions_spec.rb
@@ -5,21 +5,19 @@ RSpec.describe "search/home.html.erb", :js=>true do
     context "Form interactions" do
       before :each do
         visit home_path
+        page.choose("Custom")
       end
 
       it "Shows search options when User clicks on Custom search radio button", javascript:true do
-        page.choose("Custom")  
         expect(page).to have_selector('div.search-opts', :count=>2)
       end
 
       it "Hides search options when User clicks back on Default search" do
-        page.choose("custom")
         page.choose("Default")
         expect(page).to have_selector('div.search-opts', :count=>2, :visible=>false)
       end
 
-      it "Clears narrowed-search options when any inclusive-search box is checked" do
-        page.choose('custom') 
+      it "Clears narrowed-search options when any inclusive-search box is checked" do 
         page.fill_in('narrow-query-string', :with=>"test")  
         page.check("artist-name")
         narrow_inputs = page.all('div#narrowed-search input')
@@ -28,7 +26,6 @@ RSpec.describe "search/home.html.erb", :js=>true do
       end
 
       it "Clears inclusive-search options when user types text into narrow-search field" do
-        page.choose('custom')
         page.fill_in('narrow-query-string', :with=>"artist")
         inclusive_inputs = page.all('div#inclusive-search input')
         expect(inclusive_inputs.all?{|checkbox|
