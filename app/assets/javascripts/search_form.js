e044552e6ddb8fd5965643ecc8c47c7ecd0ab4db
authorbeard <msnolan@gmail.com>
2017-02-10 11:25:39 -0500

    added jquery processing to display results properly; currently appends new query results to previous ones.

--- app/assets/javascripts/search_form.js
+++ app/assets/javascripts/search_form.js
@@ -30,9 +30,30 @@ function tunecoreSearch(event){
   event.preventDefault();
   params = $(this).serialize()
   var search = $.post('/search', params, function(data){
-                  console.log(data)
-                }, "json")
-  search.done(function(response){
+    processResponse(data)
+  }, "json");
+
+}
+
+
+function processResponse(resp){
+  $('#results-display div.result').each(function(){
+    // this.html("")
     debugger;
+  });
+
+  $('#search-results').show();
+
+  
+  if ($.isEmptyObject(resp)){
+    return $('#search-results').html('<h1>Sorry, couldn\'t find anything.</h1>');
+  }
+  resp.forEach(function(el){
+    $('#song-title div').append('<p>' + el.name + '</p>');
+    $('#album-title div').append('<p>' + el.album['name'] + '</p>');
+    $('#artist-name div').append('<p>' + el.artist['name'] + '</p>');
+    // debugger;
+
+
   })
 }
