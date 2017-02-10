3fa5acf1fbad3988aec2d2595e26c093fd73bad9
authorbeard <msnolan@gmail.com>
2017-02-10 11:36:53 -0500

    fixed typo in search submission test

--- spec/features/search_form_submission_spec.rb
+++ spec/features/search_form_submission_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "search/home.html.erb", :js => true do
   it "Displays the search results div after user submits query" do
     visit home_path
     expect(page).to_not have_content('Search Results:')
-    fill_in('q_query-string', :with=>"whut")
+    fill_in('q_query_string', :with=>"whut")
     click_on("Search")
     expect(page).to have_content('Search Results:')
 
